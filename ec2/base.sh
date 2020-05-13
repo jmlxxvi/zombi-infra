0c5fcca85a66bf5cc38d601684c173ab4686ad16
Juan Martín Guillén <juanmartinguillen@yahoo.com.ar>
2020-05-13 14:23:38 -0300

    package.json changes

--- ec2/base.sh
+++ ec2/base.sh
@@ -27,16 +27,19 @@ sudo -u postgres psql -c "CREATE USER ${ZOMBI_DB_USER} WITH PASSWORD '${ZOMBI_DB
 sudo -u postgres psql -c "CREATE DATABASE ${ZOMBI_DB_NAME} OWNER=${ZOMBI_DB_USER};"
 
 # MySQL (MariaDB)
-sudo yum -y install mariadb-server
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+sudo yum -y install MariaDB-server galera-4 MariaDB-client MariaDB-shared MariaDB-backup MariaDB-common
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 sudo systemctl status mariadb
 sudo mysql -e "create database if not exists ${ZOMBI_DB_NAME}"
 sudo mysql -e "create user '${ZOMBI_DB_USER}'@'localhost' identified by '${ZOMBI_DB_PASS}'"
 sudo mysql -e "grant all privileges on ${ZOMBI_DB_NAME}.* to '${ZOMBI_DB_USER}'@'localhost'"
+# For standard repository version use
+#  sudo yum -y install mariadb-server
 # For latest MariaDB version use:
 #  curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
-#  sudo yum install MariaDB-server galera-4 MariaDB-client MariaDB-shared MariaDB-backup MariaDB-common
+#  sudo yum -y install MariaDB-server galera-4 MariaDB-client MariaDB-shared MariaDB-backup MariaDB-common
 # Backup/Restore example
 #  mysqldump --host myrdshost.rds.amazonaws.com --user=admin --password --lock-tables --databases my_db > my_db_full.sql
 #  mysql -h localhost -u my_user -p my_db < my_db_full.sql
@@ -47,10 +50,10 @@ sudo npm i -g pm2
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u centos --hp /home/centos
 
 # Nginx
-sudo yum -y install nginx
+#sudo yum -y install nginx
 #sudo mkdir -p /etc/tls
 #sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/tls/cert.key -out /etc/tls/cert.crt -subj "/C=AR/ST=Buenos Aires/L=Buenos Aires/O=Zombi Server Self Signed/OU=Development Server/CN=${ZOMBI_LB_HOST_NAME}"
 #sudo `which cp` ./nginx.conf /etc/nginx/nginx.conf
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo systemctl status nginx
+#sudo systemctl start nginx
+#sudo systemctl enable nginx
+#sudo systemctl status nginx
